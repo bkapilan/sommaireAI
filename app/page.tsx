15ce3aca1f577cdcfdf487d644416d086728c1bc
bkapilan <bkapilan1109@gmail.com>
2025-03-24 11:30:12 +0530

    Add lines to globals.css

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ import { Button } from "@/components/ui/button";
 export default function Home() {  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>Sommaire</h1>
       <Button variant="outline" >Shadcn Button</Button>
-      <Button variant="destructive" >Shadcn Button</Button>
-      <Button variant="secondary" >Shadcn Button</Button>
       <Button variant="link" >Shadcn Button</Button>
     </div>
   );
