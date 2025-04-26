ccee12def852578fd3e27184300b436093e2f993
bkapilan <bkapilan1109@gmail.com>
2025-04-26 20:31:42 +0530

    Summary viewer completed

--- lib/summaries.ts
+++ lib/summaries.ts
@@ -1,4 +1,4 @@
-import { getDbConnection } from "./db";
+import { getDbConnection } from "@/lib/db";
 
 export async function getSummaries(userId: string) {
     
@@ -10,3 +10,29 @@ export async function getSummaries(userId: string) {
 
     return summaries;
 }
+
+export async function getSummaryById (id: string) {
+  try {
+    const sql = await getDbConnection();
+
+    const [summary] = await sql `SELECT
+      id,
+      user_id,
+      title,
+      original_file_url,
+      summary_text,
+      status,
+      created_at,
+      updated_at,
+      file_name,
+      LENGTH(summary_text) - LENGTH(REPLACE(summary_text, ' ', '')) + 1 as word_count
+      FROM pdf_summaries where id=${id}`;
+
+    return summary;
+
+  } catch (err) {
+    console.error("Error fetching summary by id", err);
+    return null;
+  }
+}
+
